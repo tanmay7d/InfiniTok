0a19e58c320129dabf5be99cf6ffcf27d07c7a85
AsavariD <69451908+AsavariD@users.noreply.github.com>
2020-08-29 13:53:12 +0530

    New changes

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -1,7 +1,40 @@
-import React from 'react';
-import {StyleSheet, View, Text, TouchableOpacity, Button} from 'react-native';
+import React, {useLayoutEffect} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  Button,
+  Alert,
+} from 'react-native';
+import {Icon} from 'react-native-elements';
 
 const Dashboard = ({navigation}) => {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <Icon
+          type="material-community"
+          name="logout"
+          color="#fff"
+          size={40}
+          onPress={() => {
+            Alert.alert('Logout', 'Are You Sure You Want To Logout?', [
+              {
+                text: 'Yes',
+                onPress: () => {
+                  Alert.alert('Logged Out Successfully', 'Thank You');
+                },
+              },
+              {
+                text: 'No',
+                //onPress: null,
+              },
+            ]);
+          }}></Icon>
+      ),
+    });
+  }, [navigation]);
   return (
     <View>
       <Text>Hello</Text>
